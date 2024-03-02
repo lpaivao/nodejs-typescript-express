27cb59d9d400bc2f354444fe69d9074ed05b17cd
lpaivao <lpaivauefs@gmail.com>
2024-03-02 00:30:59 -0300

    Configurações do eslint

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-    return res.send('Pagina inicial funcionando!');
-})
+server.get("/", (req, res) => {
+	return res.send("Pagina inicial funcionando!");
+});
 
 export {server};
